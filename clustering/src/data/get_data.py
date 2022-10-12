3c4e6d65c8b6f1fcf96baa61e16ee1a35ad1f880
pacificus <masterkristall@gmail.com>
2022-10-13 05:07:26 +0700

    Fix style

--- clustering/src/data/get_data.py
+++ clustering/src/data/get_data.py
@@ -1,9 +1,10 @@
-import requests
-import click
-import yaml
 import logging
-import py7zr
 import os
+
+import click
+import py7zr
+import requests
+import yaml
 
 
 def extract_zip(archive_path, target_path):
@@ -13,9 +14,11 @@ def extract_zip(archive_path, target_path):
     :param archive_path: Specify the path to the archive that is to be extracted
     :param target_path: Specify the path where the extracted files will be stored
     """
-    with py7zr.SevenZipFile(archive_path, mode='r') as zip_file:
+    with py7zr.SevenZipFile(archive_path, mode="r") as zip_file:
         zip_file.extractall(path=target_path)
-    logger.info(f'Archive {archive_path} has been extracted to {target_path}...')
+    logger.info(
+        f"Archive {archive_path} has been extracted to {target_path}..."
+    )
 
 
 @click.command()
@@ -28,25 +31,25 @@ def extract_zip(archive_path, target_path):
 )
 def download_dataset(config_path):
     """
-    The download_dataset function downloads the Stack Overflow dataset from an URL and saves it to disk.
+    The download_dataset function downloads the Stack Overflow dataset from a URL and saves it to disk.
 
     :param config_path: Specify the path to the configuration file
     """
     with open(config_path) as conf_file:
         config = yaml.safe_load(conf_file)
 
-    response = requests.get(config['data']['so_dataset_url'])
-    logger.info('Dataset has been downloaded...')
+    response = requests.get(config["data"]["so_dataset_url"])
+    logger.info("Dataset has been downloaded...")
 
-    filename = 'stack_overflow_posts.7z'
+    filename = "stack_overflow_posts.7z"
     dataset_7z_path = f"{config['data']['so_dataset_path']}/{filename}"
     with open(dataset_7z_path, "wb") as f:
         f.write(response.content)
-    logger.info(f'Archive {filename} has been created...')
+    logger.info(f"Archive {filename} has been created...")
 
     extract_zip(
         archive_path=dataset_7z_path,
-        target_path=config['data']['so_dataset_path'],
+        target_path=config["data"]["so_dataset_path"],
     )
     os.remove(dataset_7z_path)
 
